7ef69cce2606a535e73677c17b52412fe59c58b2
tdeu <thomasderouck@hotmail.com>
2024-06-03 15:17:15 +0000

    Initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,14 @@
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox-viem";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.24",
+};
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.viem.getWalletClients();
+  for (const account of accounts) {
+    console.log(account.account.address);
+  }
+});
+
+export default config;
